8d5ce4651ab5b6ac344509c07fb8d5819df611b9
Erynn <vandreerynn@gmail.com>
2025-01-16 15:31:54 +0100

    Set up command

---
+++ src/Command/RefreshWebSubCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:refresh:websub',
+    description: 'Avoid the expiring pubsubhubbub',
+)]
+class RefreshWebSubCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        //SCRIPT REFRESH TOKEN//
+
+        $io->success('Refresh success !');
+
+        return Command::SUCCESS;
+    }
+}
