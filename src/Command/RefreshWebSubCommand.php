d44f54da2ea74a9be0c5d650fdf6cc418b7b05d8
Oscar <159458439+05c4rR@users.noreply.github.com>
2025-01-16 17:05:56 +0100

    update refresh command
    Co-Authored-By: Erynn <166019076+ErynnV@users.noreply.github.com>

--- src/Command/RefreshWebSubCommand.php
+++ src/Command/RefreshWebSubCommand.php
@@ -2,6 +2,9 @@
 
 namespace App\Command;
 
+use App\Entity\Channel;
+use App\Repository\ChannelRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -9,6 +12,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsCommand(
     name: 'app:refresh:websub',
@@ -16,26 +20,52 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class RefreshWebSubCommand extends Command
 {
-    public function __construct()
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private HttpClientInterface $client,
+        private ChannelRepository $channelRepository
+    )
     {
         parent::__construct();
     }
 
-    protected function configure(): void
-    {
-        $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
-    }
+    // protected function configure(): void
+    // {
+    //     $this
+    //         ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+    //         ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+    //     ;
+    // }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
-        //SCRIPT REFRESH TOKEN//
+        // SCRIPT REFRESH TOKEN 
+        $channels = $this->channelRepository->findAll();
+        // Si sur Ngrok, bien refresh l'url a chaque ouverture de port
+        $webhookUrl = 'https://9c8a-2001-861-8bb4-f960-cc62-51de-8910-880d.ngrok-free.app'."/webhook/youtube";
 
-        $io->success('Refresh success !');
+        foreach ($channels as $channel) {
+            $channelId = $channel->getChannelId();
+            $channelName = $channel->getName();
+
+            $response = $this->client->request('POST', 'https://pubsubhubbub.appspot.com/subscribe', [
+                'body' => [
+                    'hub.callback' => $webhookUrl,
+                    'hub.topic' => 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=' . $channelId,
+                    'hub.verify' => 'sync',
+                    'hub.mode' => 'subscribe'
+                ]
+            ]);
+
+            if ($response->getStatusCode() === 204) {
+                $io->success('Refresh success !');
+            } else {
+                $io->error('Renouvellement échoué pour ' . $channelName);
+            }
+        }
 
         return Command::SUCCESS;
     }
